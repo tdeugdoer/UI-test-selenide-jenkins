671bf34f9e19637a7275c8fb464a35807334dd97
tdeugdoer <tegor2003@gmail.com>
2025-04-18 17:23:53 +0300

    Fixed 25

--- src/test/java/listeners/UIListener.java
+++ src/test/java/listeners/UIListener.java
@@ -3,20 +3,17 @@ package listeners;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.awaitility.Awaitility;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import utils.CustomAwait;
 import utils.TestConstants;
 
 import java.time.Duration;
-
-import static com.codeborne.selenide.Selenide.open;
 
 public class UIListener implements ITestListener {
     @Override
@@ -24,7 +21,22 @@ public class UIListener implements ITestListener {
         Configuration.baseUrl = System.getProperty("base_url", TestConstants.Urls.BASE_URL);
         Configuration.browser = System.getProperty("browser", Browsers.EDGE);
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = System.getProperty("execution", "").equals("jenkins") ? Boolean.TRUE : Boolean.FALSE;
+
+        if (System.getProperty("execution", "").equals("jenkins") ? Boolean.TRUE : Boolean.FALSE) {
+            Configuration.headless = true;
+
+            switch (Configuration.browser.toLowerCase()) {
+                case "chrome":
+                    Configuration.browserCapabilities = getChromeOptions();
+                    break;
+                case "edge":
+                    Configuration.browserCapabilities = getEdgeOptions();
+                    break;
+            }
+
+
+        }
+
         Awaitility.setDefaultPollInterval(Duration.ofMillis(500));
         Awaitility.setDefaultPollDelay(Duration.ofMillis(500));
 
@@ -52,6 +64,37 @@ public class UIListener implements ITestListener {
     private void clearBrowser() {
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
+    }
+
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "enable-automation",
+                "--lang=en-US",
+                "--test-type",
+                "--remote-allow-origins=*",
+                "--start-maximized",
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+        );
+        return options;
+    }
+
+    private EdgeOptions getEdgeOptions() {
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments(
+                "enable-automation",
+                "--lang=en-US",
+                "--remote-allow-origins=*",
+                "--start-maximized",
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu"
+        );
+        return options;
     }
 
 }
