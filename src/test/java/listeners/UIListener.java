36edbd769a5017a92b4f417a9035177f30a50888
tdeugdoer <tegor2003@gmail.com>
2025-04-18 16:03:01 +0300

    Fixed 14

--- src/test/java/listeners/UIListener.java
+++ src/test/java/listeners/UIListener.java
@@ -3,15 +3,19 @@ package listeners;
 import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.awaitility.Awaitility;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import utils.CustomAwait;
 import utils.TestConstants;
 
 import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.open;
 
 public class UIListener implements ITestListener {
     @Override
@@ -20,7 +24,6 @@ public class UIListener implements ITestListener {
         Configuration.browser = System.getProperty("browser", Browsers.EDGE);
         Configuration.browserSize = "1920x1080";
         Configuration.headless = System.getProperty("execution", "").equals("jenkins") ? Boolean.TRUE : Boolean.FALSE;
-        System.out.println(System.getProperty("execution", "").equals("jenkins") ? Boolean.TRUE : Boolean.FALSE);
         Awaitility.setDefaultPollInterval(Duration.ofMillis(500));
         Awaitility.setDefaultPollDelay(Duration.ofMillis(500));
 
@@ -28,6 +31,15 @@ public class UIListener implements ITestListener {
                 .screenshots(Boolean.parseBoolean(System.getProperty("allure_screenshots", Boolean.TRUE.toString())))
                 .savePageSource(Boolean.parseBoolean(System.getProperty("allure_page_sources", Boolean.TRUE.toString())))
         );
+
+        CustomAwait.await().until(() -> {
+            boolean b = WebDriverRunner.hasWebDriverStarted();
+            System.out.println("----------------------------------------------------");
+            System.out.println(b);
+            System.out.println("----------------------------------------------------");
+            return b;
+        });
+        open(TestConstants.Urls.BONUS_URL);
     }
 
     @Override
